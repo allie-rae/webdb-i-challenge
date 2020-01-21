e06e49ca0558145711e88ccb223b90480bf54cec
Allie Robinson <allierob93@gmail.com>
2020-01-21 17:17:15 -0600

    PULL request working

--- accounts-router.js
+++ accounts-router.js
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({ errorMessage: "Error retrieving accounts from database." })
-        })
-})
+        });
+});
 
 router.get('/:id', (req, res) => {
     knex
@@ -27,42 +27,58 @@ router.get('/:id', (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({ errorMessage: 'Error retrieving account from database.' })
-        })
-})
+        });
+});
 
 router.post('/', validateAccount, (req, res) => {
     const postData = req.body;
     knex('accounts')
-    .insert(postData, 'id')
-    .then(ids => {
-        const id = ids[0];
-        return knex('accounts')
-        .select('id', 'name', 'budget')
-        .where({ id })
-        .first()
-        .then(account => {
-            res.status(201).json(account)
+        .insert(postData, 'id')
+        .then(ids => {
+            const id = ids[0];
+            return knex('accounts')
+                .select('id', 'name', 'budget')
+                .where({ id })
+                .first()
+                .then(account => {
+                    res.status(201).json(account)
+                })
         })
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: 'Error adding the account to the database.'})
-    })
-})
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: 'Error adding the account to the database.' })
+        });
+});
+
+router.put('/:id', validateAccount, (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    knex('accounts')
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json({ message: `${count} record(s) updated` })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "Error updating account in the database." });
+        });
+});
 
 function validateAccount(req, res, next) {
     let body = req.body;
     let name = req.body.name;
     let budget = req.body.budget;
     if (!body) {
-      res.status(400).json({ errorMessage: "Missing account data." })
+        res.status(400).json({ errorMessage: "Missing account data." })
     } else if (!name) {
-      res.status(400).json({ errorMessage: "Missing required name field." })
+        res.status(400).json({ errorMessage: "Missing required name field." })
     } else if (!budget) {
         res.status(400).json({ errorMessage: "Missing required budget field." })
     } else {
-      next();
+        next();
     };
-  }
+}
 
 module.exports = router;
